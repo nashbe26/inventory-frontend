55cf6f161bab1655a223403634d521d16c75f491
nashbe26 <alatouatiii@gmail.com>
2025-11-17 18:02:17 +0100

    inventory stablee 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://tndeals.store/api-inventory';
+const API_BASE_URL = 'https://tndeals.store/api-inventory';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
